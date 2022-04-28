dc8992bb1792196175abf8a843a8758558064ed9
Kevin Hoerr <kevin.x.hoerr@gsk.com>
2022-04-28 16:21:43 -0400

    Feature #27 Add project get-env and project set-env commands (#29)
    * Feature #27 Add project get-env and project set-env commands
    
    * Remove unnecessary parameter removal in project get-env

---
+++ src/main/java/com/ksm/domino/cli/command/project/ProjectGetEnv.java
@@ -0,0 +1,30 @@
+package com.ksm.domino.cli.command.project;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.dominodatalab.api.model.DominoCommonModelsEnvironmentVariables;
+import com.dominodatalab.api.rest.ProjectsApi;
+import com.ksm.domino.cli.command.AbstractDominoCommand;
+
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "get-env", header = "%n@|green Retrieves all environment variables for a project|@")
+public class ProjectGetEnv extends AbstractDominoCommand {
+
+    private static final String NAME = "project get-env";
+
+    @CommandLine.Parameters(description = "@|blue Required Parameters:%nprojectId=1234.....|@")
+    private final Map<String, String> parameters = new LinkedHashMap<>();
+
+    @Override
+    public void execute() throws Exception {
+        String projectId = getRequiredParam(parameters, "projectId", NAME);
+
+        ProjectsApi projectsApi = new ProjectsApi(getApiClient());
+        DominoCommonModelsEnvironmentVariables result = projectsApi.listProjectEnvironmentVariables(projectId);
+
+        output(result);
+    }
+
+}
