9bf40f57888f14d6091bce39b4475a0c4638f88c
melloware <mellowaredev@gmail.com>
2023-06-18 11:35:54 -0400

    Added TrustManager back

---
+++ src/main/java/com/ksm/domino/cli/provider/TrustAllManager.java
@@ -0,0 +1,40 @@
+package com.ksm.domino.cli.provider;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+/**
+ * Manager to allow internal Domino instances without valid SSL Certs to still work.
+ */
+public class TrustAllManager {
+
+   private static final TrustManager[] TRUST_ALL_CERTS = new TrustManager[]{new X509TrustManager() {
+      @Override
+      public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+         return null;
+      }
+
+      @Override
+      public void checkClientTrusted(final java.security.cert.X509Certificate[] certs, final String authType) {
+      }
+
+      @Override
+      public void checkServerTrusted(final java.security.cert.X509Certificate[] certs, final String authType) {
+      }
+   }};
+
+   public static SSLContext createSslContext() {
+      SSLContext sslContext;
+      try {
+         sslContext = SSLContext.getInstance("TLS");
+         sslContext.init(null, TRUST_ALL_CERTS, new SecureRandom());
+      } catch (NoSuchAlgorithmException | KeyManagementException ex) {
+         throw new RuntimeException(ex);
+      }
+      return sslContext;
+   }
+}
